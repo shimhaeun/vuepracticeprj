9d2ea8b729c089e64e158422266e61808547412f
haeun <shy624@naver.com>
2023-01-09 15:43:04 +0900

    1) 데이터 바인딩

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4>{{this.products[0]}}</h4>
+    <p>50만원</p>
+  </div>
+  <div>
+    <h4>{{this.products[1]}}</h4>
+    <p>가격은아무거나</p>
+  </div>
+  <div>
+    <h4>{{this.products[2]}}</h4>
+    <p>70만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
+  data(){
+    return {
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
